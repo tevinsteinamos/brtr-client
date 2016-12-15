31ed9219626dac6dc7bf92ca2db6efd43a6b9eb4
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-12-15 16:18:48 +0700

    remove console.llog list message

--- js/appComponents/listMessage/EachMessage.js
+++ js/appComponents/listMessage/EachMessage.js
@@ -1,55 +1,19 @@
 import React, { Component } from 'react';
 import { Image, AsyncStorage } from 'react-native';
-import { Col, Row, Grid } from 'react-native-easy-grid';
 import { connect } from 'react-redux';
 import {
-    Container,
-    Header,
-    Title,
-    Content,
-    Text, H3, H2, H1,
-    Button,
-    Icon,
-    Footer,
-    FooterTab,
-    Card,
-    CardItem,
+    Text, H3,
     Thumbnail,
-    View,
-    Input,
-    InputGroup,
-    List,
-    ListItem
+    ListItem,
 } from 'native-base';
 
-import navigateTo from '../../actions/sideBarNav';
-import { openDrawer } from '../../actions/drawer';
-import myTheme from '../../themes/base-theme';
 import styles from './styles';
-import decode from 'jwt-decode'
 import moment from 'moment'
 
 class EachMessage extends Component {
-    static propTypes = {
-        openDrawer: React.PropTypes.func,
-        navigateTo: React.PropTypes.func,
-        navigation: React.PropTypes.shape({
-            key: React.PropTypes.string,
-        }),
-
-    }
-
-    navigateTo(route, data, data2) {
-        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ini kirim id: ", data)
-        this.props.navigateTo(route, 'listMessage', data, data2);
-    }
 
     render() {
         const {items, title, itemMessageId, navigator, activeUser} = this.props
-        console.log('each item : ', items);
-        console.log('each title : ', title);
-        console.log('each itemmsg : ', itemMessageId);
-        console.log('active user : ', activeUser);
         if (activeUser.id == items.Item.User.id) {
             return (
                 <ListItem style={styles.noBottomBorder} onPress={() => navigator.push({id: 'messageDetail', itemMessageId: itemMessageId, title: title})}>
@@ -77,28 +41,17 @@ class EachMessage extends Component {
                 <ListItem style={styles.noBottomBorder} onPress={() => navigator.push({id: 'messageDetail', itemMessageId: itemMessageId, title: title})}>
                     <Thumbnail square size={90} source={{uri: items.Item.User.avatar}} />
                     <H3 style={styles.text}>DIZZY</H3>
-                        <Text note style={styles.text}>
-                            To
-                            <Text style={styles.name}>{items.Item.User.username}</Text>
-                        </Text>
-                        <Text style={{color: '#fff'}}>
-                            {moment(items.createdAt).fromNow()}
-                        </Text>
+                    <Text note style={styles.text}>
+                        To
+                        <Text style={styles.name}>{items.Item.User.username}</Text>
+                    </Text>
+                    <Text style={{color: '#fff'}}>
+                        {moment(items.createdAt).fromNow()}
+                    </Text>
                 </ListItem>
             );
         }
     }
 }
 
-function bindAction(dispatch) {
-    return {
-        openDrawer: () => dispatch(openDrawer()),
-        navigateTo: (route, homeRoute, data, data2) => dispatch(navigateTo(route, homeRoute, data, data2)),
-    };
-}
-
-const mapStateToProps = state => ({
-    navigation: state.cardNavigation,
-});
-
-export default connect(mapStateToProps, bindAction)(EachMessage);
+export default EachMessage
--- js/appComponents/listMessage/index.js
+++ js/appComponents/listMessage/index.js
@@ -2,49 +2,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { actions } from 'react-native-navigation-redux-helpers';
-import { Container, Header, Title, Content, Button, Icon, List, ListItem, Text, Thumbnail, H3 } from 'native-base';
+import {
+    Container, Header, Title, Content, Button, Icon,
+    List,
+} from 'native-base';
 import { Image, AsyncStorage, BackAndroid } from 'react-native';
 
 import styles from './styles';
 
 import myTheme from '../../themes/base-theme';
-import navigateTo from '../../actions/bottomNav';
-
-const pratik = require('../../../img/contacts/pratik.png');
-const sanket = require('../../../img/contacts/sanket.png');
-const megha = require('../../../img/contacts/megha.png');
-const atul = require('../../../img/contacts/atul.png');
-const saurabh = require('../../../img/contacts/saurabh.png');
-const varun = require('../../../img/contacts/varun.png');
 import EachMessage from './EachMessage'
-
 import {listMessageProcess} from '../../actions/listMessage'
-
 import decode from 'jwt-decode'
-
-
 
 class listMessage extends Component {
 
-  static propTypes = {
-    replaceAt: React.PropTypes.func,
-    navigateTo: React.PropTypes.func,
-    navigation: React.PropTypes.shape({
-      key: React.PropTypes.string,
-    })
-  }
+    static propTypes = {
+        replaceAt: React.PropTypes.func,
+        navigateTo: React.PropTypes.func,
+        navigation: React.PropTypes.shape({
+            key: React.PropTypes.string,
+        })
+    }
 
-  constructor(props) {
-      super(props);
-      this.state = {
-          tab1: false,
-          tab2: true,
-          tab3: false,
-          dataUser: {},
-          token: '',
-          messages: []
-      };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            tab1: false,
+            tab2: true,
+            tab3: false,
+            dataUser: {},
+            token: '',
+            messages: []
+        };
+    }
 
     componentWillMount() {
         BackAndroid.addEventListener('hardwareBackPress', () => {
@@ -55,47 +46,41 @@ class listMessage extends Component {
     }
 
     _loadInitialState = async () => {
-      try {
-          var value = await AsyncStorage.getItem("myKey");
-          console.log("value: ", value)
-          if (value !== null){
-              this.setState({token: value});
-              this.setState({dataUser: decode(value)});
-              if (value) {
-                this.props.listMessageProcess(value)
-              }
-              this._appendMessage('Recovered selection from disk: ' + value);
-          } else {
-              console.log("else")
-              this._appendMessage('Initialized with no selection on disk.');
-          }
-      } catch (error) {
-          console.log("catch")
-          this._appendMessage('AsyncStorage error: ' + error.message);
-      }
+        try {
+            var value = await AsyncStorage.getItem("myKey");
+            if (value !== null){
+                this.setState({token: value});
+                this.setState({dataUser: decode(value)});
+                if (value) {
+                    this.props.listMessageProcess(value)
+                }
+                this._appendMessage('Recovered selection from disk: ' + value);
+            } else {
+                this._appendMessage('Initialized with no selection on disk.');
+            }
+        } catch (error) {
+            this._appendMessage('AsyncStorage error: ' + error.message);
+        }
     }
 
     _appendMessage = (message) => {
         this.setState({messages: this.state.messages.concat(message)});
     };
 
-  render() {
-    const {item, navigator} = this.props
-    console.log('item lis : ', item);
-    console.log('user : ', this.state.dataUser);
-    let ItemNodes = item.map((data)=> {
-      if (this.state.dataUser.id == data.Item.UserId || this.state.dataUser.id == data.Item.UserId) {
-        console.log('user detected : ', data);
-        return(
-            <EachMessage key={data.id} items={data} title={data.title} itemMessageId={data.id} navigator={navigator} activeUser={this.state.dataUser}/>
-        )
-      } else {
-      }
-    })
+    render() {
+        const {item, navigator} = this.props
+        let ItemNodes = item.map((data)=> {
+            if (this.state.dataUser.id == data.Item.UserId || this.state.dataUser.id == data.Item.UserId) {
+                return(
+                    <EachMessage key={data.id} items={data} title={data.title} itemMessageId={data.id} navigator={navigator} activeUser={this.state.dataUser}/>
+                )
+            } else {
+            }
+        })
 
-    return (
-      <Container theme={myTheme} style={styles.container}>
-          <Header>
+        return (
+            <Container theme={myTheme} style={styles.container}>
+                <Header>
                     <Title style={{alignSelf: 'center', color: '#6CF9C8'}}>MESSAGES</Title>
                     <Button transparent onPress={() => navigator.pop()}>
                         <Icon name="ios-arrow-back" />
@@ -105,24 +90,24 @@ class listMessage extends Component {
                     </Button>
                 </Header>
 
-        <Content>
-          <List>
-            {ItemNodes}
-          </List>
-        </Content>
-      </Container>
-    );
-  }
+                <Content>
+                    <List>
+                        {ItemNodes}
+                    </List>
+                </Content>
+            </Container>
+        );
+    }
 }
 
 function bindAction(dispatch) {
-  return {
-    listMessageProcess: (token) => dispatch(listMessageProcess(token)),
-  };
+    return {
+        listMessageProcess: (token) => dispatch(listMessageProcess(token)),
+    };
 }
 
 const mapStateToProps = state => ({
-  item: state.listMessage
+    item: state.listMessage
 });
 
 export default connect(mapStateToProps, bindAction)(listMessage);
