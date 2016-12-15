bea72a11603028b9cfa1063460e2e94c29007cca
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-12-15 20:00:30 +0700

    remoe all spinner

--- js/appComponents/profileDetail/index.js
+++ js/appComponents/profileDetail/index.js
@@ -126,8 +126,8 @@ class ProfileDetail extends Component {
         }
         else {
             buttonLogout =
-            <Button
-                bordered style={{
+                <Button
+                    bordered style={{
                           alignSelf: 'center',
                           marginTop: 40,
                           marginBottom: 20 ,
@@ -136,8 +136,8 @@ class ProfileDetail extends Component {
                           borderColor:'#2effd0',
                           height: 50
                     }}
-                onPress={this.logoutUser.bind(this)}><Text style={{color: '#FFFFFF'}}>LOGOUT</Text>
-            </Button>
+                    onPress={this.logoutUser.bind(this)}><Text style={{color: '#FFFFFF'}}>LOGOUT</Text>
+                </Button>
         }
 
 
@@ -147,56 +147,48 @@ class ProfileDetail extends Component {
             )
         })
 
-        if(this.state.loading) {
-            return(
-                <Container theme={myTheme} style={styles.container}>
-                    <Content>
-                        <Spinner color='green' />
-                    </Content>
-                </Container>
-            )
-        }
-        else {
-            return (
-                <Container theme={myTheme} style={styles.container}>
-                    <Header>
-                        <Title style={{alignSelf: 'center', color: '#6CF9C8'}}>
-                            {(this.props.route.UserId) ? ((this.props.route.UserId === this.state.dataUser.id) ? 'MY PROFILE' : `${user.username} PROFILE` ) : 'MY PROFILE'}
-                        </Title>
-                        <Button transparent onPress={() => this.props.navigator.push({id: 'searchItem'})}>
-                            <Icon name="ios-search"/>
-                        </Button>
-                        <Button transparent
-                                onPress={() => this.props.navigator.push({id: 'editProfile', avatar: user.avatar})}>
-                            <Icon name="ios-settings"/>
-                        </Button>
-                    </Header>
 
-                    <Content>
-                        <Card style={{ flex: 0, backgroundColor: '#1E1E1E', borderWidth: 0 }}>
-                            <CardItem
-                                style={{borderBottomWidth: 0}}
-                            >
-                                <Image
-                                    style={{resizeMode: 'cover',  alignSelf: 'center', width: 200, height: 200 }}
-                                    source={(user.avatar) ? {uri: user.avatar} : require('../../../img/img-placeholder.png')}
-                                />
-                            </CardItem>
-                        </Card>
-                        <Text
-                            style={{
+
+        return (
+            <Container theme={myTheme} style={styles.container}>
+                <Header>
+                    <Title style={{alignSelf: 'center', color: '#6CF9C8'}}>
+                        {(this.props.route.UserId) ? ((this.props.route.UserId === this.state.dataUser.id) ? 'MY PROFILE' : `${user.username} PROFILE` ) : 'MY PROFILE'}
+                    </Title>
+                    <Button transparent onPress={() => this.props.navigator.push({id: 'searchItem'})}>
+                        <Icon name="ios-search"/>
+                    </Button>
+                    <Button transparent
+                            onPress={() => this.props.navigator.push({id: 'editProfile', avatar: user.avatar})}>
+                        <Icon name="ios-settings"/>
+                    </Button>
+                </Header>
+
+                <Content>
+                    <Card style={{ flex: 0, backgroundColor: '#1E1E1E', borderWidth: 0 }}>
+                        <CardItem
+                            style={{borderBottomWidth: 0}}
+                        >
+                            <Image
+                                style={{resizeMode: 'cover',  alignSelf: 'center', width: 200, height: 200 }}
+                                source={(user.avatar) ? {uri: user.avatar} : require('../../../img/img-placeholder.png')}
+                            />
+                        </CardItem>
+                    </Card>
+                    <Text
+                        style={{
                             color: '#fff',
                             alignSelf: 'center',
                             fontSize: 20,
                             fontStyle: 'normal',
                             marginBottom: 20}}>
-                            {user.username}
-                        </Text>
+                        {user.username}
+                    </Text>
 
-                        {buttonLogout}
+                    {buttonLogout}
 
-                        <Text
-                            style={{
+                    <Text
+                        style={{
                             color: '#fff',
                              alignSelf: 'center',
                              fontSize: 20,
@@ -204,33 +196,33 @@ class ProfileDetail extends Component {
                              marginTop: 20,
                              marginBottom: 20
                         }}>
-                        </Text>
+                    </Text>
 
-                        <List>
-                            {ItemNodes}
-                        </List>
-                    </Content>
+                    <List>
+                        {ItemNodes}
+                    </List>
+                </Content>
 
-                    <Footer>
-                        <FooterTab>
-                            <Button
-                                active={this.state.tab1} onPress={() => navigator.replace({id: 'home'})}>
-                                <Icon name='md-home'/>
-                            </Button>
-                            <Button active={this.state.tab2} onPress={() => navigator.replace({id: 'addItem'})}>
+                <Footer>
+                    <FooterTab>
+                        <Button
+                            active={this.state.tab1} onPress={() => navigator.replace({id: 'home'})}>
+                            <Icon name='md-home'/>
+                        </Button>
+                        <Button active={this.state.tab2} onPress={() => navigator.replace({id: 'addItem'})}>
 
-                                <Icon name='md-add-circle'/>
-                            </Button>
-                            <Button active={this.state.tab3} onPress={() => navigator.replace({id: 'profileDetail'})}>
+                            <Icon name='md-add-circle'/>
+                        </Button>
+                        <Button active={this.state.tab3} onPress={() => navigator.replace({id: 'profileDetail'})}>
 
-                                <Icon name='ios-person'/>
-                            </Button>
-                        </FooterTab>
-                    </Footer>
+                            <Icon name='ios-person'/>
+                        </Button>
+                    </FooterTab>
+                </Footer>
 
-                </Container>
-            );
-        }
+            </Container>
+        );
+
     }
 }
 
